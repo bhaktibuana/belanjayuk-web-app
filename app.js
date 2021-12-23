2a2dc8f1c3cf73b41bcdcbded4aedbbf0771beef
bhaktibuana <bhaktibuana19@gmail.com>
2021-12-23 20:40:18 +0700

    add bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const ejs = require("ejs");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send("Hi, welcome to Belanjayuk :)");
+// ejs configuration
+app.set("view engine", "html");
+app.engine("html", ejs.renderFile);
+app.use(expressLayouts);
+
+app.get("/", (req, res) => {
+  res.render('pages/landingPage', {
+    layout: "./layouts/index",
+    title: "Belanjayuk",
+  });
 });
 
 app.listen(port, () => {
